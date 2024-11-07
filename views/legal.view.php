c590143b09b78b5a98088759a5c9aadf3cf529e3
Amaury Franssen <amauryfranssen@gmail.com>
2024-11-07 23:50:57 +0100

    fix: error in home

--- views/legal.view.php
+++ views/legal.view.php
@@ -2,12 +2,12 @@
     header('HTTP/1.0 403 Forbidden', TRUE, 403);
     die();
 } ?>
-<main class="min-h-screen mx-auto">
+<main class="min-h-screen mx-auto max-w-[1200px]">
 <div className="mx-auto px-5 container">
-      <div className="mx-auto max-w-[800px] bg-white shadow-md mx-auto my-5 p-6 rounded-lg max-w-4xl">
+      <div className="my-5 mx-auto max-w-[800px] bg-white shadow-md mx-auto my-5 p-6 rounded-lg max-w-4xl">
         <h1 className="mb-4 font-bold text-3xl text-gray-800">Fausses Mentions Légales</h1>
 
-        <section className="mb-6">
+        <section className="my-5">
           <h2 className="mb-2 font-semibold text-gray-700 text-xl">Éditeur du site</h2>
           <p className="text-gray-600">
             <strong>MVC Pokemon</strong>, société de présentation des pokemon avec une architecture MVC, est une société SAS au capital social de 0 euros, dont le siège social est situé au
@@ -19,20 +19,20 @@
           </p>
         </section>
 
-        <section className="mb-6">
+        <section className="my-5">
           <h2 className="mb-2 font-semibold text-gray-700 text-xl">Directeur de la publication</h2>
           <p className="text-gray-600">Amaury Franssen et Nassim Aissaoui</p>
           <a href="mailto:">pokemon-fake@yuamail.com</a>
         </section>
 
-        <section className="mb-6">
+        <section className="my-5">
           <h2 className="mb-2 font-semibold text-gray-700 text-xl">Hébergement</h2>
           <p className="text-gray-600">
             Ce site n'est hébergé null part à ce stade
           </p>
         </section>
 
-        <section class="mb-6">
+        <section class="my-5">
           <h2 class="mb-2 font-semibold text-gray-700 text-xl">Garantie et sécurité</h2>
           <p class="text-gray-600">
             Le contenu de ce site est édité sous réserve d’erreurs techniques et/ou typographiques, avec des photos non contractuelles.
