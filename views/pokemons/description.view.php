beef631aabe3a6616d411ff597cb504a409c08e1
Amaury Franssen <amauryfranssen@gmail.com>
2024-11-10 23:58:00 +0100

    feat: adjust size of the description card

--- views/pokemons/description.view.php
+++ views/pokemons/description.view.php
@@ -28,21 +28,20 @@ if($_GET['id'] !== null && $_GET['id'] !== "") {
             <div class="my-5 flex flex-col gap-3 items-center justify-center">
                 <h1 class="my-5 text-xl font-bold text-center"><?= $pokemonName ?></h1>
             </div>
-            <div class="mx-auto flex justify-between gap-24"> 
-                <article class="group/card relative min-h-[350px] bg-white flex flex-col justify-between items-center 
+            <div class="mx-auto flex justify-between gap-24 p-3"> 
+                <article class="group/card relative max-w-[75%] min-h-[350px] bg-white flex flex-col justify-between items-center 
                 p-5 rounded-lg border border-2 border-purple-700 transition-colors duration-300 ease-in hover:bg-yellow-500">
                     <div class="flex flex items-center justify-center top-bar absolute w-1/2 bg-purple-600 top-0 left-1/2 transform -translate-x-1/2 rounded-b-lg">
                         <p class="text-white text-center mt-2 mb-3 text-sm font-semibold">
                             <?php echo $pokemonName ?? "Aucun nom" ?>
                         </p>
                     </div>
-                    <div class="pt-10 grow content flex flex-col justify-center items-center">
-                        <div class="w-1/2 h-1/2">
+                    <div class="pt-10 content flex flex-col justify-center items-center">
+                        <div class="w-[350px] h-auto flex items-center justify-center">
                             <img src="<?php echo 'public/img/pokemons/' . $pokemonImage . '.png' ?? 'pikachu' . '.png'; ?>"
                                 alt="<?php echo $pokemonName ?? "pikachu"; ?>"
-                                class="w-full h-full rounded-full object-cover object-top">
+                                class="w-full object-contain object-center">
                         </div>
-                      
                     </div>
                     <div class="box-footer mt-3 flex flex-col gap-5">
                         <p class="text-center"><?php echo $pokemonType ?? "Aucun type" ?></p>
